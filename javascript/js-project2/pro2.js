0f1b37b1005e073a5afe17fe7bb68be78f53b6a1
vspkumai <vspkumari09@gmail.com>
2024-05-02 21:06:29 +0530

    update

---
+++ javascript/js-project2/pro2.js
@@ -0,0 +1,31 @@
+let colorselect= document.getElementById("color-select");
+let body= document.body;
+let btntext=document.getElementById('btn');
+/*colorselect.addEventListener('change', function() {
+  const selectedColor = this.value;
+  body.style.backgroundColor = selectedColor;
+});*/
+
+
+function change(){
+
+  const selectedcolor=colorselect.value;
+  document.body.style.backgroundColor= selectedcolor;
+  
+
+}
+function changemode()
+{
+  document.body.classList.toggle('dark')
+   
+  if(btntext.textContent == "🌙")
+  {
+    btn.textContent= "⛅";
+  }
+  else{
+    btn.textcontent= "🌙"
+  }
+
+}
+
+
